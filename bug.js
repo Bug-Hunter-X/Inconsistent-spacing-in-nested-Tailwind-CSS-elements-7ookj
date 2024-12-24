49c0a4d5c743252ea5b06e3132169f037223dc9a
BugHunter-X <askzenai@gmail.com>
2024-12-24 23:28:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+//In your component file
+<div className="bg-gray-200 p-4">
+  <p className="text-lg font-bold">This is a paragraph</p>
+  <ul className="list-disc">
+    <li className="ml-6">List item 1</li>
+    <li className="ml-6">List item 2</li>
+    <li className="ml-6">List item 3</li>
+  </ul>
+</div>
+```
